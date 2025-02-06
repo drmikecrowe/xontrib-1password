32ded5523ccba4b86fa33b5eb7f90ec68db2259f
Andy Kipp <anki-code@users.noreply.github.com>
2025-02-06 22:21:04 +0600

    Update main.py

--- xontrib_1password/main.py
+++ xontrib_1password/main.py
@@ -17,18 +17,17 @@ class OnePass:
         return self.__repr__()
 
     def __repr__(self):
-        cache = __xonsh__.env.get("XONTRIB_1PASSWORD_CACHE", "not_empty")
         if __xonsh__.env.get("XONTRIB_1PASSWORD_ENABLED", True):
-            if self.url not in _1password_cache:
-                result = subprocess.run(["op", "read", self.url], capture_output=True, text=True)
-                value = result.stdout.strip()
-                if result.stderr:
-                    print(f"xontrib-1password: {self.url!r}: {result.stderr}", file=sys.stderr)
+            cache = __xonsh__.env.get("XONTRIB_1PASSWORD_CACHE", "not_empty")
+            if self.url in _1password_cache and cache not in ['off', False]:
+                return _1password_cache[self.url]
 
-                if cache == "all" or (cache == "not_empty" and value):
-                    if __xonsh__.env.get("XONTRIB_1PASSWORD_DEBUG", False):
-                        print(f"xontrib-1password: added {self.url!r} value to cache", file=sys.stderr)
-                    _1password_cache[self.url] = value
+            value = self.op_read(self.url)
+            
+            if cache == "all" or (cache == "not_empty" and value):
+                if __xonsh__.env.get("XONTRIB_1PASSWORD_DEBUG", False):
+                    print(f"xontrib-1password: added {self.url!r} value to cache", file=sys.stderr)
+                _1password_cache[self.url] = value
             return value
         else:
             if self.url in _1password_cache:
@@ -36,3 +35,9 @@ class OnePass:
                     print(f"xontrib-1password: removed {self.url!r} value from cache.", file=sys.stderr)
                 del _1password_cache[self.url]
             return self.url
+
+    def op_read(self, url):
+        result = subprocess.run(["op", "read", url], capture_output=True, text=True)
+        value = result.stdout.strip()
+        if result.stderr:
+            print(f"xontrib-1password: {url!r}: {result.stderr}", file=sys.stderr)
